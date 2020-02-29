d4516f9d400d85b1efc8d43768e3c3545f5c40ce
nor-ko-hi-jp <saandna111@gmail.com>
2020-02-29 22:39:24 +0900

    test: add deepCopy test

---
+++ __tests__/utils/deep-copy.util.spec.ts
@@ -0,0 +1,43 @@
+import { deepCopy } from '../../src/utils'
+
+describe('deepCopy', () => {
+  it('return not object if input isnot object', () => {
+    const result = deepCopy(1)
+    expect(result).toEqual(1)
+  })
+
+  it('return array if input is array', () => {
+    const input = [1, 2, 3, 4]
+    const result = deepCopy(input)
+    result.push(5)
+    expect(input.length).toEqual(4)
+    expect(result.length).toEqual(5)
+  })
+
+  it('return object if input object (not array)', () => {
+    const input = {
+      no: 1,
+      fruitNames: ['pine', 'apple'],
+      fruitPrices: [220, 150],
+      description: {
+        important: 'important',
+        warning: 'warning'
+      }
+    }
+
+    const result = deepCopy(input)
+    result.no = 2
+    result.fruitPrices.push(300)
+    result.fruitNames.push('orange')
+    result.description.important = 'warning'
+
+    expect(input.no).toEqual(1)
+    expect(result.no).toEqual(2)
+    expect(input.fruitPrices.length).toEqual(2)
+    expect(result.fruitPrices.length).toEqual(3)
+    expect(input.fruitNames.length).toEqual(2)
+    expect(result.fruitNames.length).toEqual(3)
+    expect(input.description.important).toEqual('important')
+    expect(result.description.important).toEqual('warning')
+  })
+})
