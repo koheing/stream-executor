6a649744d6e86039fdc3219bcd38a524823f6898
nor-ko-hi-jp <saandna111@gmail.com>
2020-03-01 17:53:16 +0900

    fix: custom types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,14 @@
-export type PickKeyValue<T, V> = {
+type PickKeyValue<T, V> = {
   [K in keyof T]: T[K] extends V ? K : never
 }[keyof T]
 
-export type OmitFunction<T> = T extends Array<any>
-  ? T
-  : T extends { [key: string]: any }
-  ? Omit<T, PickKeyValue<T, Function>>
-  : T
+type IsArray<T, U> = T extends Array<any> ? T : U
+type IsNotObject<T, U> = T extends { [key: string]: any } ? U : T
+type PickFunction<T> = PickKeyValue<T, Function>
+
+export type OmitFunction<T> = IsArray<
+  T,
+  IsNotObject<T, Omit<T, PickFunction<T>>>
+>
 
 export type Action<T, U> = (value: T) => U
