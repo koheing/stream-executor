b0eac9840bb47939582b9edcbb3cfe41b24959e9
nor-ko-hi-jp <saandna111@gmail.com>
2020-03-01 10:47:13 +0900

    fix: suggest

--- lib/executors/executor.facade.d.ts
+++ lib/executors/executor.facade.d.ts
@@ -1,12 +1,11 @@
 import { Action, WithoutGetterAndFunction } from '../types';
 import { ChainExecutor } from './chain.executor';
 import { ParallelExecutor } from './parallel.executor';
-declare type WithoutGetterAndFunctionOr<U> = U extends U ? U : WithoutGetterAndFunction<U>;
 export declare class StreamExecutorFacade<T> {
     private _initialValue;
     constructor(initialValue: T, doDeepCopy: boolean);
-    chain<A, B, C, D, E, F, G, H, I, J>(act1: Action<WithoutGetterAndFunctionOr<T>, A>, act2?: Action<A, B>, act3?: Action<B, C>, act4?: Action<C, D>, act5?: Action<D, E>, act6?: Action<E, F>, act7?: Action<F, G>, act8?: Action<G, H>, act9?: Action<H, I>, act10?: Action<I, J>): Pick<ChainExecutor<WithoutGetterAndFunctionOr<T>>, "execute">;
-    parallel<A, B, C, D, E, F, G, H, I, J>(act1: Action<WithoutGetterAndFunctionOr<T>, A>, act2?: Action<WithoutGetterAndFunctionOr<T>, B>, act3?: Action<WithoutGetterAndFunctionOr<T>, C>, act4?: Action<WithoutGetterAndFunctionOr<T>, D>, act5?: Action<WithoutGetterAndFunctionOr<T>, E>, act6?: Action<WithoutGetterAndFunctionOr<T>, F>, act7?: Action<WithoutGetterAndFunctionOr<T>, G>, act8?: Action<WithoutGetterAndFunctionOr<T>, H>, act9?: Action<WithoutGetterAndFunctionOr<T>, I>, act10?: Action<WithoutGetterAndFunctionOr<T>, J>): Pick<ParallelExecutor<WithoutGetterAndFunctionOr<T>>, "execute">;
+    chain<A, B, C, D, E, F, G, H, I, J>(act1: Action<WithoutGetterAndFunction<T>, A>, act2?: Action<A, B>, act3?: Action<B, C>, act4?: Action<C, D>, act5?: Action<D, E>, act6?: Action<E, F>, act7?: Action<F, G>, act8?: Action<G, H>, act9?: Action<H, I>, act10?: Action<I, J>): Pick<ChainExecutor<WithoutGetterAndFunction<T>>, "execute">;
+    parallel<A, B, C, D, E, F, G, H, I, J>(act1: Action<WithoutGetterAndFunction<T>, A>, act2?: Action<WithoutGetterAndFunction<T>, B>, act3?: Action<WithoutGetterAndFunction<T>, C>, act4?: Action<WithoutGetterAndFunction<T>, D>, act5?: Action<WithoutGetterAndFunction<T>, E>, act6?: Action<WithoutGetterAndFunction<T>, F>, act7?: Action<WithoutGetterAndFunction<T>, G>, act8?: Action<WithoutGetterAndFunction<T>, H>, act9?: Action<WithoutGetterAndFunction<T>, I>, act10?: Action<WithoutGetterAndFunction<T>, J>): Pick<ParallelExecutor<WithoutGetterAndFunction<T>>, "execute">;
 }
 /**
  * create streamer
@@ -14,4 +13,3 @@ export declare class StreamExecutorFacade<T> {
  * @param doDeepCopy default `true`. don't deepCody if `false`.
  */
 export declare const createStream: <T>(initialValue: T, doDeepCopy?: boolean) => StreamExecutorFacade<T>;
-export {};
