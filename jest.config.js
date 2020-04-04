98e3e0ee01d01699c62460b61b94f71973e845e9
nor-ko-hi-jp <saandna111@gmail.com>
2020-04-04 12:57:01 +0900

    fix: [jest] can pass test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)',
+    '**/?(*.)+(test).ts?(x)',
+    '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  ],
   transformIgnorePatterns: ['node_modules'],
+  modulePaths: ['src'],
   globals: {
     'ts-jest': {
-      babelConfig: true
-    }
-  }
+      babelConfig: true,
+    },
+  },
 }
